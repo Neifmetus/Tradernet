b454302861d8ba85cf863a7ffa7d04ecee7c7373
Ekaterina <bateeva-k@mail.ru>
2022-10-30 23:05:05 +0300

    Initial Commit

---
+++ Tradernet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tradernet
+//
+//  Created by Екатерина Батеева on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
